1971dc8397f528072da5fed239516131ce668e4d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-13 07:40:05 +0200

    Config resource example

---
+++ tests/resources/Project/Config.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Example\Client\Resource\Project;
+
+use WyriHaximus\ApiClient\Annotations\Nested;
+use WyriHaximus\ApiClient\Resource\TransportAwareTrait;
+
+abstract class Config implements ConfigInterface
+{
+    use TransportAwareTrait;
+
+    /**
+     * @var string
+     */
+    protected $a;
+
+    /**
+     * @var string
+     */
+    protected $b;
+
+    /**
+     * @var string
+     */
+    protected $c;
+
+    /**
+     * @var string
+     */
+    protected $d;
+
+    /**
+     * @return string
+     */
+    public function a() : string
+    {
+        return $this->a;
+    }
+
+    /**
+     * @return string
+     */
+    public function b() : string
+    {
+        return $this->b;
+    }
+
+    /**
+     * @return string
+     */
+    public function c() : string
+    {
+        return $this->c;
+    }
+
+    /**
+     * @return string
+     */
+    public function d() : string
+    {
+        return $this->d;
+    }
+}
