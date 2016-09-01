c39c24f279c983fce03bef3dc687d9eb73b7c226
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-01 22:52:19 +0200

    Annotation handlers

---
+++ src/AnnotationHandler/Collection.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tools\ResourceGenerator\AnnotationHandler;
+
+use ApiClients\Foundation\Hydrator\Annotations\Collection as CollectionAnnotation;
+use ApiClients\Tools\ResourceGenerator\AnnotationHandlerInterface;
+
+class Collection implements AnnotationHandlerInterface
+{
+    /**
+     * @param array $yaml
+     * @return array
+     */
+    public static function handle(string $property, array $yaml, $input): array
+    {
+        $yaml['annotations']['Collection'][$property] = $input;
+        $yaml['uses'][CollectionAnnotation::class] = true;
+
+        return $yaml;
+    }
+}
