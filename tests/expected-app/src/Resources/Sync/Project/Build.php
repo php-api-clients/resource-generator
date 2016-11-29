8ba79578516b2825c099f5b7a33fe651dcaf5c65
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-29 22:27:54 +0100

    Handle promise coming from the commands handler

--- tests/expected-app/src/Resources/Sync/Project/Build.php
+++ tests/expected-app/src/Resources/Sync/Project/Build.php
@@ -4,11 +4,14 @@ namespace Example\Client\Resource\Sync\Project;
 
 use ApiClients\Foundation\Hydrator\CommandBus\Command\BuildAsyncFromSyncCommand;
 use Example\Client\Resource\Project\Build as BaseBuild;
+use Example\Client\Resource\Project\BuildInterface;
 
 class Build extends BaseBuild
 {
     public function refresh() : Build
     {
-        return $this->wait($this->handleCommand(new BuildAsyncFromSyncCommand('Project\\Build', $this)));
+        return $this->wait($this->handleCommand(new BuildAsyncFromSyncCommand(self::HYDRATE_CLASS, $this))->then(function (BuildInterface $build) {
+            return $build->refresh();
+        }));
     }
 }
